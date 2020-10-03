e307cec1579104a4638b9d2e673e81cb32659cd4
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-10-03 14:52:58 +0530

    Added : Show Board Method Added to Display Current Board

--- com/capgemini/workshop/tictactoe/TicTacToeGame.java
+++ com/capgemini/workshop/tictactoe/TicTacToeGame.java
@@ -17,12 +17,14 @@ public class TicTacToeGame {
 			computer = 'O';
 		else
 			computer = 'X';
+		
+		game.showBoard(board);
 	}
 
 	// Create Board and Assign Empty Spaces
 	private char[] createBoard() {
 		char [] board = new char[10];
-		for(int i=1; i<10; i++)
+		for(int i=1; i<board.length; i++)
 			board[i] = ' ';
 		return board;
 	}
@@ -32,5 +34,18 @@ public class TicTacToeGame {
 		System.out.println("Choose Your Letter X or O");
 		char userChoice = sc.next().charAt(0);
 		return userChoice;
+	}
+	// Show Board
+	private void showBoard(char[] board) {
+		int k = 1;
+		for(int i=0; i<=2;i++) {
+			System.out.println("\n_______\n");
+			System.out.print("|");
+			for(int j=0; j<=2; j++) {
+				System.out.print(board[k]+"|");
+				k++;
+			}
+		}
+		System.out.println("\n_______");
 	}
 }
