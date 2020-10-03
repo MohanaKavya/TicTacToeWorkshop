f2a1010e92e7c8aa9160fbb0c28b94d6b85feee0
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-10-03 13:59:29 +0530

    Added : Functionality to Enable User to Choose Letter is Added

--- com/capgemini/workshop/tictactoe/TicTacToeGame.java
+++ com/capgemini/workshop/tictactoe/TicTacToeGame.java
@@ -1,16 +1,36 @@
 package com.capgemini.workshop.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {		
 
 	public static void main(String[] args) {
+		//variables
+		char computer = ' ';
+		
 		TicTacToeGame game = new TicTacToeGame();
-		game.createBoard();
+		char [] board = game.createBoard();
+		char userChoice = game.chooseUserLetter();
+		
+		// Assigning Letter to Computer
+		if(userChoice == 'X')
+			computer = 'O';
+		else
+			computer = 'X';
 	}
 
 	// Create Board and Assign Empty Spaces
-	private void createBoard() {
+	private char[] createBoard() {
 		char [] board = new char[10];
 		for(int i=1; i<10; i++)
 			board[i] = ' ';
+		return board;
+	}
+	// Selection of Letter by User 
+	private char chooseUserLetter()
+	{	Scanner sc = new Scanner(System.in);
+		System.out.println("Choose Your Letter X or O");
+		char userChoice = sc.next().charAt(0);
+		return userChoice;
 	}
 }
