bb5baa77157c587ae4be266a729829962f66ad64
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-10-03 15:22:21 +0530

    Added : Functionality to Enable User to Choose Index is Added

--- com/capgemini/workshop/tictactoe/TicTacToeGame.java
+++ com/capgemini/workshop/tictactoe/TicTacToeGame.java
@@ -7,10 +7,9 @@ public class TicTacToeGame {
 	public static void main(String[] args) {
 		//variables
 		char computer = ' ';
-		
-		TicTacToeGame game = new TicTacToeGame();
-		char [] board = game.createBoard();
-		char userChoice = game.chooseUserLetter();
+	
+		char [] board = createBoard();
+		char userChoice = chooseUserLetter();
 		
 		// Assigning Letter to Computer
 		if(userChoice == 'X')
@@ -18,25 +17,44 @@ public class TicTacToeGame {
 		else
 			computer = 'X';
 		
-		game.showBoard(board);
+		showBoard(board);
+		userSelectIndex(board, userChoice);
 	}
+	// Select Index for Player
+	private static void userSelectIndex(char[] board, char userChoice) {
+		Scanner sc = new Scanner(System.in);
+		boolean available = false;
+		do {
+		System.out.println("Choose a Desired Location on Board from 1 to 9 :");
+		int position = sc.nextInt();
+		if(board[position]==' ') {
+			board[position] = userChoice;
+			available = true;
+		}
+		else 
+			System.out.println("Invalid Position or Position Occupied. Try Again");
+		}while(available==false);
+		
+		showBoard(board);	
+	}
+		
 
 	// Create Board and Assign Empty Spaces
-	private char[] createBoard() {
+	private static char[] createBoard() {
 		char [] board = new char[10];
 		for(int i=1; i<board.length; i++)
 			board[i] = ' ';
 		return board;
 	}
 	// Selection of Letter by User 
-	private char chooseUserLetter()
+	private static char chooseUserLetter()
 	{	Scanner sc = new Scanner(System.in);
 		System.out.println("Choose Your Letter X or O");
 		char userChoice = sc.next().charAt(0);
 		return userChoice;
 	}
 	// Show Board
-	private void showBoard(char[] board) {
+	private static void showBoard(char[] board) {
 		int k = 1;
 		for(int i=0; i<=2;i++) {
 			System.out.println("\n_______\n");
